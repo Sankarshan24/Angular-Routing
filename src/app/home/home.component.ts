8254abd73570c6c0ccc84c280e496a78310a9bd0
SankarshanRamesh <Sankarshanr@gmail.com>
2023-06-14 14:48:40 +0530

    using relative paths for routing and use of navigate()

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onLoadServers() {
+    this.router.navigate(['/servers']);
   }
 
+  onLoadServerDetails() {
+    this.router.navigate(['servers'], { relativeTo: this.activatedRoute });
+  }
 }
